224b2fb2b8c30a38c88ca55a9ac7e50452178516
Rohith04MVK <rohithkbobby04@gmail.com>
2023-09-22 23:41:29 +0530

    Added tests for layers and activations with their docs

--- gradphi/nn/activations/relu.py
+++ gradphi/nn/activations/relu.py
@@ -1,31 +1,85 @@
 import numpy as np
 from gradphi.autodiff import Variable
 
-# class ReLU():
-#     def __init__(self):
-#         self.input_data = None
 
-#     def forward(self, x):
-#         self.input_data = x
-#         f_x = np.maximum(0, x)
-#         return f_x
+class ReLU(Variable):
+    """
+    A class representing the Rectified Linear Unit (ReLU) activation function.
 
-#     def backward(self, grad=1):
-#         if self.input_data is None:
-#             raise ValueError("Forward must be called before backward.")
-#         return grad * (self.input_data > 0)
+    The Rectified Linear Unit (ReLU) is a widely used activation function in neural networks.
+    It introduces non-linearity by transforming the input data such that negative values
+    are replaced with zero, while positive values remain unchanged. The ReLU activation is
+    defined as follows:
 
-#     def __call__(self, x):
-#         return self.forward(x)
+    ReLU(x) = max(0, x)
 
-class ReLU(Variable): #Dynamic ReLU computation (slower than static)
+    Attributes:
+    ----------
+    None
+
+    Methods:
+    --------
+    - forward(self, x):
+      Compute the forward pass of the ReLU activation function.
+
+      Parameters:
+      -----------
+      x : numpy.ndarray
+          The input data to which the ReLU activation will be applied.
+
+      Returns:
+      --------
+      numpy.ndarray
+          The result of the ReLU activation applied to the input data 'x'.
+
+    - backward(self, grad=1):
+      Compute the backward pass of the ReLU activation function.
+
+      Parameters:
+      -----------
+      grad : float, optional
+          An optional gradient value that can be provided. Default is 1.
+
+      Returns:
+      --------
+      None
+          This method prints the gradient information but does not
+          perform gradient computation in this implementation.
+
+    - __call__(self, x):
+      Allow using the ReLU instance as a callable object for the forward pass.
+
+      Parameters:
+      -----------
+      x : numpy.ndarray
+          The input data to which the ReLU activation will be applied.
+
+      Returns:
+      --------
+      numpy.ndarray
+          The result of the ReLU activation applied to the input data 'x'.
+
+    Usage:
+    ------
+    Create an instance of this class and use it to apply the ReLU activation function
+    to your input data. For example:
+
+    ```python
+    relu = ReLU()
+    input_data = numpy.array([-2.0, 0.5, 3.0, -1.5])
+    output = relu(input_data)
+    ```
+
+    The output will be: `[0.  0.5 3.  0. ]`
+    """
+
     def __init__(self):
         pass
 
     def forward(self, x):
         self.input_data = x
         return x.maximum(0)
-    
+
     def backward(self, grad=1):
         print(self.input_data.backward(np.ones_like(self.input_data.data)))
         return self.input_data.backward()
--- gradphi/nn/layers/dense.py
+++ gradphi/nn/layers/dense.py
@@ -1,9 +1,87 @@
 import numpy as np
-
 from gradphi.autodiff import Variable
 
 
-class Dense():
+class Dense:
+    """
+    A class representing a Dense (fully connected) layer in a neural network.
+
+    The Dense layer, also known as a fully connected layer, performs linear transformations
+    on input data. It is typically used in neural networks to connect every input neuron
+    to every output neuron with trainable weights and optional bias.
+
+    The forward pass computes the linear transformation as follows:
+    output = X * weight^T + bias
+
+    Parameters:
+    -----------
+    - in_features : int
+        The number of input features or input neurons.
+    - out_features : int
+        The number of output features or output neurons.
+    - bias : bool, optional (default=True)
+        Whether to include bias terms in the layer.
+
+    Attributes:
+    ----------
+    - weight : Variable
+        The learnable weight matrix of shape (out_features, in_features).
+    - bias : Variable or None
+        The optional bias vector of shape (1, out_features). None if bias is not used.
+
+    Methods:
+    --------
+    - forward(self, X):
+      Compute the forward pass of the Dense layer.
+
+      Parameters:
+      -----------
+      X : Variable
+          The input data or input features of shape (batch_size, in_features).
+
+      Returns:
+      --------
+      Variable
+          The result of the linear transformation applied to the input data.
+
+    - backward(self, grad=1):
+      Compute the backward pass of the Dense layer.
+
+      Parameters:
+      -----------
+      grad : float or numpy.ndarray, optional (default=1)
+          The gradient of the loss with respect to the output of this layer.
+
+      Returns:
+      --------
+      None
+          This method updates the gradients of the weight and bias if present,
+          but does not return any values.
+
+    - __call__(self, X):
+      Allow using the Dense layer instance as a callable object for the forward pass.
+
+      Parameters:
+      -----------
+      X : Variable
+          The input data or input features of shape (batch_size, in_features).
+
+      Returns:
+      --------
+      Variable
+          The result of the linear transformation applied to the input data.
+
+    Usage:
+    ------
+    Create an instance of this class to add a Dense layer to your neural network. For example:
+
+    ```python
+    dense_layer = Dense(in_features=64, out_features=128, bias=True)
+    input_data = Variable(np.random.randn(32, 64))  # Batch size of 32, 64 input features
+    output = dense_layer(input_data)
+    ```
+    """
+
     def __init__(self, in_features, out_features, bias=True):
         # Initialize the super class (Variable)
         super().__init__(None, [], None)
