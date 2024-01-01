c9ee5214ef7d8326569bb6e52d9487e4e3cda47c
Rohith04MVK <rohithkbobby04@gmail.com>
2024-01-01 23:58:49 +0530

    Fixed sigmoid activation

---
+++ gradphi/nn/activations/sigmoid.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from gradphi.autodiff import Variable
+
+
+class Sigmoid(Variable):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def __call__(self, x):
+        return self.forward(x)
